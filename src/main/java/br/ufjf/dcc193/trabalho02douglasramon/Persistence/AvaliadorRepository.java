acfac70ef8ef153420361d8e850c7d8f131ccbb5
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-05-22 22:34:34 -0300

    Criando classe AvaliadorRepository

---
+++ src/main/java/br/ufjf/dcc193/trabalho02douglasramon/Persistence/AvaliadorRepository.java
@@ -0,0 +1,10 @@
+package br.ufjf.dcc193.trabalho02douglasramon.Persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.ongplus.Models.Avaliador;
+
+
+
+public interface AvaliadorRepository extends JpaRepository<Avaliador, Long>{
+}
