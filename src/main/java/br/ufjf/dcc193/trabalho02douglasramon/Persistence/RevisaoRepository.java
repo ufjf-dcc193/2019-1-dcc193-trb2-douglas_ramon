c215002fe383ff723ff9b0f4d0ad9dcacbb18eae
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-05-22 22:35:48 -0300

    Criando classe RevisaoRepository

---
+++ src/main/java/br/ufjf/dcc193/trabalho02douglasramon/Persistence/RevisaoRepository.java
@@ -0,0 +1,10 @@
+package br.ufjf.dcc193.trabalho02douglasramon.Persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.ongplus.Models.Revisao;
+
+
+
+public interface RevisaoRepository extends JpaRepository<Revisao, Long>{
+}
