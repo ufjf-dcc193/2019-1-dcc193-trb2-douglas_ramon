a4eddc8fd1a502d1deeb72ec912eab1205783918
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-06-19 01:48:38 -0300

    Melhorando layout

--- src/main/java/br/ufjf/dcc193/trabalho02douglasramon/Controllers/RevisaoController.java
+++ src/main/java/br/ufjf/dcc193/trabalho02douglasramon/Controllers/RevisaoController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
